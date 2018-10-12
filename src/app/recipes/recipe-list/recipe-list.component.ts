62bdfc0e4ad2ba6af062e0c797b1c254174ae2d6
Prasanth Nair <prasanth.nair@aig.com>
2018-10-11 23:36:42 -0400

    v2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../../model/recipe-model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test recipe',
+    'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
+    new Recipe('Test Recipe', 'This is a test recipe',
+    'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg')
+  ];
+
+ 
+
+  constructor() {
+    console.log('inside constructor');
+    console.log(this.recipes);
+   }
 
   ngOnInit() {
   }
