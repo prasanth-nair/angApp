5fed95d255a01c70192c259b75b0bce025b6ccbb
Prasanth Nair <prasanth.nair@aig.com>
2018-10-12 21:04:00 -0400

    Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../model/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+   ingredients: Ingredients[] =
+   [
+      new Ingredients('Apple', 3),
+      new Ingredients('Orange', 2),
+      new Ingredients('Banana', 5),
+
+   ];
+
 
   constructor() { }
 
